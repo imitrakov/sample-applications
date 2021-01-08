0371bb0fd42331d8e04130d06bf90f486abc727e
imitrakov <iamitrakov@gmail.com>
2021-01-08 22:21:56 +0300

    [TRAINING_TELEGRAM_BOT] Add commands from message, add dockerfile and gitignore

--- tg-money-bot/main.py
+++ tg-money-bot/main.py
@@ -1,8 +1,9 @@
 import logging
+import os
 
 from aiogram import Bot, Dispatcher, executor, types
 
-API_TOKEN = 'BOT TOKEN HERE'
+API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -14,14 +15,49 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    """
-    This handler will be called when user sends `/start` or `/help` command
-    """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+    """Отправляет приветственное сообщение и помощь по боту"""
+    await message.answer(
+        "Бот для учёта финансов\n\n"
+        "Добавить расход: 250 такси\n"
+        "Сегодняшняя статистика: /today\n"
+        "За текущий месяц: /month\n"
+        "Последние внесённые расходы: /expenses\n"
+        "Категории трат: /categories")
+
+
+@dp.message_handler(lambda message: message.text.startswith('/del'))
+async def del_expense(message: types.Message):
+    """Удаляет одну запись о расходе по её идентификатору"""
+    await message.answer(message.text)
+
+
+@dp.message_handler(commands=['categories'])
+async def categories_list(message: types.Message):
+    """Отправляет список категорий расходов"""
+    await message.answer(message.text)
+
+
+@dp.message_handler(commands=['today'])
+async def today_statistics(message: types.Message):
+    """Отправляет сегодняшнюю статистику трат"""
+    await message.answer(message.text)
+
+
+@dp.message_handler(commands=['month'])
+async def month_statistics(message: types.Message):
+    """Отправляет статистику трат текущего месяца"""
+    await message.answer(message.text)
+
+
+@dp.message_handler(commands=['expenses'])
+async def list_expenses(message: types.Message):
+    """Отправляет последние несколько записей о расходах"""
+    await message.answer(message.text)
 
 
 @dp.message_handler()
-async def echo(message: types.Message):
+async def add_expense(message: types.Message):
+    """Добавляет новый расход"""
     await message.answer(message.text)
 
 
