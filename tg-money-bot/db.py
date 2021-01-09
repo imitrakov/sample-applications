5cf825cd0bc55b0890c2ebccdabc4c89ceef7cf6
imitrakov <iamitrakov@gmail.com>
2021-01-09 23:44:59 +0300

    [TRAINING_TELEGRAM_BOT] Add db schema and initialize sqlite

---
+++ tg-money-bot/db.py
@@ -0,0 +1,26 @@
+import os
+import sqlite3
+
+conn = sqlite3.connect(os.path.join("db", "finance.db"))
+cursor = conn.cursor()
+
+
+def _init_db():
+    """Инициализирует БД"""
+    with open("createdb.sql", "r") as f:
+        sql = f.read()
+    cursor.executescript(sql)
+    conn.commit()
+
+
+def check_db_exists():
+    """Проверяет, инициализирована ли БД, если нет — инициализирует"""
+    cursor.execute("SELECT name FROM sqlite_master "
+                   "WHERE type='table' AND name='expense'")
+    table_exists = cursor.fetchall()
+    if table_exists:
+        return
+    _init_db()
+
+
+check_db_exists()
